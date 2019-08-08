a57da77bf9bf741403f4f99adf978dac693b8b1d
Abraham Ho <whoabe@gmail.com>
2019-08-08 15:00:25 -0700

    adding build folder for production

---
+++ splits-app/build/precache-manifest.570f9bf620b97544883d0266fcfd8ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1076e0361a7fed207fe5889dfcd1f833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351b7b84b37aa5176e2f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ff194ca664918a2803bb",
+    "url": "/static/css/main.d0abfceb.chunk.css"
+  },
+  {
+    "revision": "351b7b84b37aa5176e2f",
+    "url": "/static/js/2.c08a6906.chunk.js"
+  },
+  {
+    "revision": "ff194ca664918a2803bb",
+    "url": "/static/js/main.75a03b3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9a758523a11e5e2dd590345ddf5f2ed",
+    "url": "/static/media/loadingcolorbar.f9a75852.gif"
+  }
+]);
